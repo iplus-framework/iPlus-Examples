903bcd43a48336e9f0700bf2fb5da850b351ab9f
Damir Lisak <dlisak@kajbum.com>
2025-08-15 18:36:06 +0200

    Remote control for Agent added in example project

--- 01_MyContextAndWF/gip.iplus.client/Masterpage.xaml.cs
+++ 01_MyContextAndWF/gip.iplus.client/Masterpage.xaml.cs
@@ -355,6 +355,12 @@ namespace gip.iplus.client
             return Dispatcher.Invoke(DispatcherPriority.Normal, action);
         }
 
+        public object DispatcherInvokeRemoteCmd(Action action, string acUrl, IACInteractiveObject obj = null, bool isMethodInvoc = true)
+        {
+            RemoteCommandManager.Instance.AddNewRemoteCommand(obj, acUrl, isMethodInvoc);
+            return DispatcherInvoke(action);
+        }
+
         public void StartBusinessobjectByACCommand(ACCommand acCommand)
         {
             if (DockingManager == null)
